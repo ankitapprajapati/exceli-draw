1fb708496b056a919886aa9b28fbc9ab1ab5e6a1
Ankit Prajapati <ankitapprajapati6268@gmail.com>
2025-02-20 22:33:22 +0530

    update schema

--- packages/zod-common/src/types.ts
+++ packages/zod-common/src/types.ts
@@ -1,6 +1,14 @@
+import { x } from 'joi'
 import {z} from 'zod'
 
 export const signinSchema = z.object({
-    email      :     z.string(),
+    email      :     z.string().email(),
     password    :    z.string(),
+})
+
+export const signupSchema = z.object({
+    email    : z.string().email(),
+    password : z.string(),
+    firstName : z.string(),
+    lastName    : z.string(), 
 })
