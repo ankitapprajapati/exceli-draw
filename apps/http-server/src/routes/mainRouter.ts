7a378edd958e35c0aad7848acdc072fb4687d6b5
Ankit Prajapati <ankitapprajapati6268@gmail.com>
2025-03-02 23:15:18 +0530

    ..

--- apps/http-server/src/routes/mainRouter.ts
+++ apps/http-server/src/routes/mainRouter.ts
@@ -5,4 +5,4 @@ import { roomRouter } from "./roomRouter";
 export const mainRouter:Router = express.Router();
 
 mainRouter.use( "/user",userRouter )
-mainRouter.use( "/room",roomRouter)
+mainRouter.use( "/room", roomRouter)
